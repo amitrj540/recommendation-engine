5d56a20dbd38615286f77a2805c891ebdbbea6b3
amitrj540 <amitrj444@gmail.com>
2021-02-03 23:27:04 +0530

    Added collborative (item-item) model

---
+++ models/collaborative-item-item.py
@@ -0,0 +1,38 @@
+import pickle
+
+import pandas as pd
+from surprise import KNNWithMeans
+from surprise import Dataset
+from surprise import accuracy
+from surprise import Reader
+from surprise.model_selection import train_test_split
+
+
+# Getting the new dataframe which contains users who has given 50 or more ratings
+def item_item_collaborative(df_path, limit):
+    df = pd.read_json(df_path)
+    new_df = df[df['review_count'] >= limit]
+    # Reading the dataset
+    reader = Reader(rating_scale=(1, 5))
+    data = Dataset.load_from_df(new_df[['reviewerID', 'asin', 'overall']], reader)
+    # Splitting the dataset
+    train_set, test_set = train_test_split(data, test_size=0.3, random_state=101)
+    # Use user_based true/false to switch between user-based or item-based collaborative filtering
+    print('Parameter Tuning...')
+    acc_score = []
+    for i in range(1, 10):
+        algo = KNNWithMeans(k=i, sim_options={'name': 'pearson_baseline', 'user_based': False})
+        algo.fit(train_set)
+        acc_score.append((i, accuracy.rmse(algo.test(test_set))))
+    # run the trained model against the testset
+    acc_score.sort(reverse=True, key=lambda x: x[1])
+    c = acc_score[0][0]
+    print(f"Final C = {c}\nTest RMSE : {acc_score[0][1]}")
+    final_knn = KNNWithMeans(k=c, sim_options={'name': 'pearson_baseline', 'user_based': False})
+    final_knn.fit(data)
+
+    print("Storing model in pickle file ...")
+    final_knn_file = 'final_knn.pkl'
+    pickle.dump(final_knn, open(final_knn_file, 'wb'))
+    print('Done.')
+    return True
--- recommendation_filters/popularity_filter.py
+++ recommendation_filters/popularity_filter.py
@@ -1,22 +1,21 @@
 import pandas as pd
 
-#avg_rev_count = df['review_count'].mean()
-#avg_rating = df['overall'].mean()
-#avg_sentiment = df['reviewText_senti'].mean()
 
-def popularity(df_path, avg_rev_count, avg_rating, avg_sentiment):
+# avg_rev_count = df['review_count'].mean()
+# avg_rating = df['overall'].mean()
+# avg_sentiment = df['reviewText_senti'].mean()
+def popularity(df_path, rev_count, rating, sentiment):
     df = pd.read_json(df_path)
-    df = df.loc[:, ['asin', 'overall', 'review_count', 'title']]
-    df1 = df.loc[:, ['asin', 'title']].drop_duplicates().reset_index().drop('index', axis=1)
+    df = df.loc[:, ['asin', 'overall', 'review_count', 'reviewText_senti', 'positive_prob']]
+    # df1 = df.loc[:, ['asin', 'title']].drop_duplicates().reset_index().drop('index', axis=1)
     pop_prod = df.groupby('asin').mean()
-    df1.merge(pop_prod, on='asin')
-    pop_prod = pop_prod[(pop_prod['overall'] >= avg_rating) &
-                        (pop_prod['review_count'] >= avg_rev_count) &
-                        (pop_prod['reviewText_senti'] >= avg_sentiment)]
-    ## Sorting Best products
+    # df1.merge(pop_prod, on='asin')
+    pop_prod = pop_prod[(pop_prod['overall'] >= rating) &
+                        (pop_prod['review_count'] >= rev_count) &
+                        (pop_prod['reviewText_senti'] >= sentiment)]
+    # Sorting Best products
     best_prod = pop_prod.sort_values('positive_prob', ascending=False)
     # Most Popular Products
     most_pop = best_prod.groupby('asin').mean().sort_values('positive_prob', ascending=False)
-    most_pop = df1.merge(most_pop, on='asin', index=False)
-    return most_pop[['asin', 'title']]
-
+    # most_pop = df1.merge(most_pop, on='asin', index=False)
+    return most_pop[['asin']]
